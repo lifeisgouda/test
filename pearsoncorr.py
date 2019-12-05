8d93b1edc3231b49ba48e480778f8b7f9e257962
janejeon <dahaejeon@gmail.com>
2019-12-05 18:19:06 +0900

    pearsoncorr

---
+++ pearsoncorr.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb    # Graph
+
+
+
+
+def read_csv(self, file_name):
+    input_data = pd.read_csv("abc.csv")
+    print(input_data.head())
+    
+def pearson_correlation_coefficient(self, a, b):
+    pearsoncorr = input_data.corr(method='pearson')
+    print(pearsoncorr)
+
+def pearsoncorr_graph():
+    sb.heatmap(
+        pearsoncorr, 
+        xticklabels = pearsoncorr.columns,
+        yticklabels = pearsoncorr.columns,
+        cmap = 'RdBu_r',
+        annot = True,
+        linewidth = 0.5
+    )
